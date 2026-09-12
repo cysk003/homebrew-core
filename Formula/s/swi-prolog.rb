560814c895572e4dcaf845d1be932032945d468c
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-09-13 00:34:18 +0900

    swi-prolog: fix build in sandbox

--- Formula/s/swi-prolog.rb
+++ Formula/s/swi-prolog.rb
@@ -1,10 +1,31 @@
 class SwiProlog < Formula
   desc "ISO/Edinburgh-style Prolog interpreter"
   homepage "https://www.swi-prolog.org/"
-  url "https://www.swi-prolog.org/download/stable/src/swipl-10.0.2.tar.gz"
-  sha256 "e42cc098f7b8a6051c4f79a99b55162d467098aba60f69649bdc7583f0734b57"
   license "BSD-2-Clause"
   head "https://github.com/SWI-Prolog/swipl-devel.git", branch: "master"
+
+  stable do
+    url "https://www.swi-prolog.org/download/stable/src/swipl-10.0.2.tar.gz"
+    sha256 "e42cc098f7b8a6051c4f79a99b55162d467098aba60f69649bdc7583f0734b57"
+
+    # TODO: remove the resource and patch when the stable release includes the
+    # prebuilt icon (devel 10.1.13+)
+    on_macos do
+      resource "swipl.icns" do
+        url "https://raw.githubusercontent.com/SWI-Prolog/swipl-devel/05262941a70720fefdd16f649f03e50fe2b069d6/desktop/swipl.icns"
+        sha256 "022530bf60d40ae425a32d7758d4bfc1896b2b99e3238a6a051d68bd49397562"
+      end
+
+      # Backport of shipping a prebuilt macOS icon instead of running `iconutil`, which the
+      # build sandbox does not allow. `patch` cannot apply the binary icon in the commit
+      # (GNU patch rejects git binary diffs, Apple's skips them), so it comes as a resource.
+      patch do
+        url "https://github.com/SWI-Prolog/swipl-devel/commit/05262941a70720fefdd16f649f03e50fe2b069d6.patch?full_index=1"
+        sha256 "7d0c7ebfad36ef48c8511c3caa1a4a042d62438155e8377f70dd79288947173a"
+        type :backport
+      end
+    end
+  end
 
   livecheck do
     url "https://www.swi-prolog.org/download/stable/src/"
@@ -41,6 +62,7 @@ class SwiProlog < Formula
   def install
     # Remove bundled libraries
     rm_r("packages/libedit/libedit")
+    (buildpath/"desktop").install resource("swipl.icns") if OS.mac? && build.stable?
 
     args = %W[
       -DSWIPL_PACKAGES_GUI=OFF
