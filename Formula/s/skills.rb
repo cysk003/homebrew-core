e006d755cf569d1e4d9b20b74d363f05a3afe4ac
Patrick Linnane <patrick@linnane.io>
2026-09-13 00:18:10 +0000

    skills: update 1.5.26 bottle.

--- Formula/s/skills.rb
+++ Formula/s/skills.rb
@@ -6,7 +6,7 @@ class Skills < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "59c852ee5f1b373806814acaa0487237a107128d8a73459b45b308d2ae4e799c"
+    sha256 cellar: :any_skip_relocation, all: "06a6941b9d2e14ff94590f735f0115ca1e7203c4e52d5ada8536e23c1bad47dd"
   end
 
   depends_on "node"
